61e74f162991cd994a45e8bbd1cb4172a6f33843
r7projects-shayan <shayansworks@gmail.com>
2024-07-12 01:15:17 +0530

    basics and functions updated

--- course/app1/main.py
+++ course/app1/main.py
@@ -1,2 +1,54 @@
-import streamlit as sl
+import streamlit as st
 
+st.title("This is the first streamlit app")
+
+st.subheader("this is a subheader")
+
+st.text("this is a text which is normal")
+
+st.header("This is the header")
+
+# Markdown
+st.markdown(
+    "**bold text in markdown**"
+)
+
+#json 
+
+jsonobject = {
+    "a":"1",
+    "b":"2",
+    "c":"3"
+}
+
+st.json(jsonobject)
+
+#code 
+
+codevar = '''
+
+def func1(param1, param2):
+  print("hello world")
+
+'''
+
+st.code(codevar)
+
+#Add stylings to all these
+
+st.write('''# this is h1 tag
+        
+         ''')
+
+#matrix
+
+st.metric(label = "wind speed", value = "170", delta="1.4ms")
+
+#here delta means the change in wind speed
+
+
+#chatbox
+
+hello = st.chat_input("enter something")
+
+st.write(hello)
